c651a45df7d5438a83aca28e0e8947d020c16d50
alanreji2004 <alenreji2004@gmail.com>
2025-07-09 19:48:27 +0530

    file structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./pages/Home/Home"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+])
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div>
+      Incepto 25
+    </div>
+  )
+}
+
+export default Home
